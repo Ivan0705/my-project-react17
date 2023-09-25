1a6db25cffecba32a1fd26b0ee067ec246ac409b
Ivan <vipanda569@mail.ru>
2023-09-25 17:35:34 +0700

    Add eslint

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 import {App} from "./app/App";
 import {BrowserRouter} from "react-router-dom";
 import {ThemeProvider} from "./app/providers/ThemeProvider";
-//import "./shared/config/i18n/i18n";
- 
+import "./shared/config/i18n/i18n";
+
 render(
     <BrowserRouter>
         <ThemeProvider>
