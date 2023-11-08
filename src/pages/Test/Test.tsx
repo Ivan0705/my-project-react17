2643b3a0378bd55e1dcc91dbd20a94cb592e2778
Ivan <vipanda569@mail.ru>
2023-11-08 17:00:06 +0700

    Added refresh plugin, added concurrently

---
+++ src/pages/Test/Test.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import {useTranslation} from "react-i18next";
+import {classNames, Mods} from "../../shared/lib/classNames/classNames";
+import cls from './Test.module.scss'
+
+interface TestProps {
+    className?: string
+}
+
+export const Test = (props: TestProps) => {
+    const {className} = props;
+    
+    const mods: Mods = {};
+
+    return (
+        <div className={classNames(cls.Test, mods, [className])}>
+            Hello test!
+        </div>
+    )
+};
